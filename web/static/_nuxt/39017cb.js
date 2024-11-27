c9af17907da2acba47027df45e51fad015d791e5
Владимир Мацюк <62064707+Wgmlgz@users.noreply.github.com>
2024-11-28 00:57:32 +0300

    enhanced password sequrity

---
+++ web/static/_nuxt/39017cb.js
@@ -0,0 +1,43 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[0], {
+  285: function(C, t, r) {
+      "use strict";
+      r.r(t);
+      var L = {
+          props: {
+              color: {
+                  type: String,
+                  required: !1,
+                  default: "black"
+              }
+          }
+      }
+        , e = (r(823),
+      r(28))
+        , component = Object(e.a)(L, (function() {
+          var C = this
+            , t = C._self._c;
+          return t("svg", )
+      }
+      ), [], !1, null, "382aa60b", null);
+      t.default = component.exports
+  },
+  458: function(C, t, r) {
+      var content = r(824);
+      content.__esModule && (content = content.default),
+      "string" == typeof content && (content = [[C.i, content, ""]]),
+      content.locals && (C.exports = content.locals);
+      (0,
+      r(105).default)("41b6af72", content, !0, {
+          sourceMap: !1
+      })
+  },
+  823: function(C, t, r) {
+      "use strict";
+      r(458)
+  },
+  824: function(C, t, r) {
+      var L = r(104)(!1);
+      L.push([C.i, ".letter[data-v-382aa60b]{fill:none;stroke:var(--color);fill-rule:nonzero;stroke-width:.57px}.letter-inner[data-v-382aa60b]{fill:var(--color)}.logo[data-v-382aa60b]:hover{transform:scale(1.04)}", ""]),
+      C.exports = L
+  }
+}]);
