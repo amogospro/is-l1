9cba12988600f147fed40a0f68b8afe82bf4467c
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-12-13 22:19:13 +0300

    Update ProductService.java

--- backend/src/main/java/com/lab1/lab1/service/ProductService.java
+++ backend/src/main/java/com/lab1/lab1/service/ProductService.java
@@ -16,6 +16,9 @@ import jakarta.transaction.Transactional;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.management.relation.Role;
 
 @ApplicationScoped
 public class ProductService {
@@ -32,58 +35,86 @@ public class ProductService {
     private UserRepository userRepository;
 
     private boolean simulateError = false;
+    
+    private static final ConcurrentHashMap<String, Boolean> productNameCache = new ConcurrentHashMap<>();
+
+    private boolean isProductNameTaken(String name) {
+        return productNameCache.putIfAbsent(name, Boolean.TRUE) == null;
+    }
+
+    private void releaseProductName(String name) {
+        productNameCache.remove(name);
+    }
 
     @Transactional
     public void createProduct(Product product, User user) throws Exception {
-
+        
         if (simulateError) {
             throw new RuntimeException("Simulated Database failure");
         }
 
-        Person owner = product.getOwner();
-
-        if (owner.getId() != 0) {
-            // Если передан id, то ищем владельца по id
-            Person existingOwner = personRepository.findById(owner.getId());
-
-
-            if (existingOwner == null) {
-                throw new Exception("Product owner not found");
-            }
-
-
-            product.setOwner(existingOwner);
-        }
-
-        Organization organization = product.getManufacturer();
-
-        if (organization != null) {
-            if (organization.getId() != null) {
-                Organization existingOrganization = organizationRepository.findById(organization.getId());
-
-                System.out.println(existingOrganization);
-                if (existingOrganization == null) {
-                    throw new Exception("Manufacturer not found");
-                }
-
-                product.setManufacturer(existingOrganization);
-            }
-        }
-
-
-
-        product.setUserOwner(user);
-        if (productRepository.findByName(product.getName()) != null) {
+        if (!isProductNameTaken(product.getName())) {
             throw new Exception("Продукт с таким именем уже существует");
         }
-        productRepository.create(product);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        String updateJson = objectMapper.writeValueAsString(ProductMapper.toDTO(product));
+        try {
+            Person owner = product.getOwner();
 
-        // Отправляем данные асинхронно через WebSocket
-        WebSocketService.sendUpdateAsync(updateJson);
+            if (owner.getId() != 0) {
+                // Если передан id, то ищем владельца по id
+                Person existingOwner = personRepository.findById(owner.getId());
+
+
+                if (existingOwner == null) {
+                    throw new Exception("Product owner not found");
+                }
+
+
+                product.setOwner(existingOwner);
+            }
+
+            Organization organization = product.getManufacturer();
+
+            if (organization != null) {
+                if (organization.getId() != null) {
+                    Organization existingOrganization = organizationRepository.findById(organization.getId());
+
+                    System.out.println(existingOrganization);
+                    if (existingOrganization == null) {
+                        throw new Exception("Manufacturer not found");
+                    }
+
+                    product.setManufacturer(existingOrganization);
+                }
+            }
+
+
+
+            product.setUserOwner(user);
+
+            if (productRepository.findByName(product.getName()) != null) {
+                throw new Exception("Продукт с таким именем уже существует");
+            }
+            productRepository.create(product);
+
+            if (productRepository.findByName(product.getName()) == null) {
+                System.out.println("how");
+            } else {
+                System.out.println("impostor?");
+            }
+            
+            productRepository.create(product);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            String updateJson = objectMapper.writeValueAsString(ProductMapper.toDTO(product));
+
+            // Отправляем данные асинхронно через WebSocket
+            WebSocketService.sendUpdateAsync(updateJson);
+        } catch (Exception e) {
+            releaseProductName(product.getName());
+            throw e;
+        }
     }
 
     public Product getProductById(int id) {
@@ -92,50 +123,63 @@ public class ProductService {
 
     @Transactional
     public void updateProduct(Product product, User user) throws Exception {
-        Product currentProduct = getProductById(product.getId());
-        // Check if user is owner or admin
-        if (currentProduct.getUserOwner().equals(user) || user.getRole() == Role.ADMIN) {
-            currentProduct.setName(product.getName());
-            currentProduct.setCoordinates(product.getCoordinates());
-            currentProduct.setCreationDate(product.getCreationDate());
-            currentProduct.setUnitOfMeasure(product.getUnitOfMeasure());
+        String new_name = product.getName();
+        if (!isProductNameTaken(new_name)) {
+            throw new Exception("Продукт с таким именем уже существует");
+        }
 
-            Organization organization = product.getManufacturer();
-            if (organization != null) {
-                if (organization.getId() != null) {
-                    Organization existingOrganization = organizationRepository.findById(organization.getId());
-                    if (existingOrganization == null) {
-                        throw new Exception("Manufacturer not found");
+        try {
+            Product currentProduct = getProductById(product.getId());
+            String old_name = currentProduct.getName();
+
+            // Check if user is owner or admin
+            if (currentProduct.getUserOwner().equals(user) || user.getRole() == Role.ADMIN) {
+                currentProduct.setName(product.getName());
+                currentProduct.setCoordinates(product.getCoordinates());
+                currentProduct.setCreationDate(product.getCreationDate());
+                currentProduct.setUnitOfMeasure(product.getUnitOfMeasure());
+
+                Organization organization = product.getManufacturer();
+                if (organization != null) {
+                    if (organization.getId() != null) {
+                        Organization existingOrganization = organizationRepository.findById(organization.getId());
+                        if (existingOrganization == null) {
+                            throw new Exception("Manufacturer not found");
+                        }
+                        currentProduct.setManufacturer(existingOrganization);
                     }
-                    currentProduct.setManufacturer(existingOrganization);
+                } else {
+                    currentProduct.setManufacturer(null);
                 }
+
+                Person owner = product.getOwner();
+                if (owner.getId() != 0) {
+                    Person existingOwner = personRepository.findById(owner.getId());
+                    if (existingOwner == null) {
+                        throw new Exception("Owner not found");
+                    }
+                    currentProduct.setOwner(existingOwner);
+                }
+
+
+                currentProduct.setPrice(product.getPrice());
+                currentProduct.setManufactureCost(product.getManufactureCost());
+                currentProduct.setRating(product.getRating());
+                productRepository.update(currentProduct);
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.registerModule(new JavaTimeModule());
+                String updateJson = objectMapper.writeValueAsString(ProductMapper.toDTO(currentProduct));
+
+                // Отправляем данные асинхронно через WebSocket
+                WebSocketService.sendUpdateAsync(updateJson);
             } else {
-                currentProduct.setManufacturer(null);
+                throw new Exception("Access Denied");
             }
-
-            Person owner = product.getOwner();
-            if (owner.getId() != 0) {
-                Person existingOwner = personRepository.findById(owner.getId());
-                if (existingOwner == null) {
-                    throw new Exception("Owner not found");
-                }
-                currentProduct.setOwner(existingOwner);
-            }
-
-
-            currentProduct.setPrice(product.getPrice());
-            currentProduct.setManufactureCost(product.getManufactureCost());
-            currentProduct.setRating(product.getRating());
-            productRepository.update(currentProduct);
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.registerModule(new JavaTimeModule());
-            String updateJson = objectMapper.writeValueAsString(ProductMapper.toDTO(currentProduct));
-
-            // Отправляем данные асинхронно через WebSocket
-            WebSocketService.sendUpdateAsync(updateJson);
-        } else {
-            throw new Exception("Access Denied");
+            releaseProductName(old_name);
+        } catch (Exception e) {
+            releaseProductName(new_name);
+            throw e;
         }
     }
 
@@ -158,6 +202,8 @@ public class ProductService {
                 throw new Exception("Access Denied");
             }
         }
+
+        releaseProductName(product.getName());
     }
 
     public List<Product> getAllProducts(int page, int size, String filterBy, String filter, String sortBy, String sortDirection) {
